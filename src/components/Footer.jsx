c371420c315bb67a1990b07876eb717518460c91
SantanuLayek <santanulayek2021@gmail.com>
2024-07-05 23:42:17 +0530

    Changed Logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     <Section crosses className="!px-0 !py-10">
       <div className="container flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col">
         <p className="caption text-violet-300 lg:block">{new Date().getFullYear()}.</p>
-        <p className="caption text-emerald-300 lg:block">Project Team Members : <i>Santanu Layek, Gauransh Sharma, Vihan Aggarwal, Adhiraj Singh Chauhan</i></p>
+        <p className="caption text-emerald-300 lg:block">Project Team Members : <i>Santanu Layek, Gauransh Sharma, Vihan Aggarwal</i></p>
       </div>
     </Section>
   )
